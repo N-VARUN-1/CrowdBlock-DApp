63bfee019f1a43ca4781c24dc651a9207a507e31
NANDURI VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-01-22 01:05:47 +0530

    Update Index.js

--- server/Index.js
+++ server/Index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import authRoutes from './Routes/auth.route.js';
 import campaignRoutes from './Routes/campaign.route.js';
@@ -12,7 +13,7 @@ dotenv.config();
 const app = express();
 
 // CORS
-
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.urlencoded({ 
