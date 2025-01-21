6075bd5d0ffe6f629a873609129bd8e7ee650626
NANDURI VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-01-21 21:36:58 +0530

    Update Index.js

--- server/Index.js
+++ server/Index.js
@@ -39,10 +39,6 @@ app.get('/', (req, res) => {
     res.send('CrowdBlock Backend is Running!');
 });
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log(`Server running on port 3000`)
-})
-
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/', {
     useNewUrlParser: true,
     useUnifiedTopology: true
