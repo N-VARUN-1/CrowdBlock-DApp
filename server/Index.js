c58c29b95b8e789a50495feeb924d14399a49c82
NANDURI VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-01-23 12:46:35 +0530

    Update Index.js

--- server/Index.js
+++ server/Index.js
@@ -13,7 +13,11 @@ dotenv.config();
 const app = express();
 
 // CORS
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
+}));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.urlencoded({ 
