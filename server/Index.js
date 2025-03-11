29b50b32c8048e4a3b9264786cd2b8bd7274ed83
NANDURI VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-03-12 01:33:41 +0530

    Update Index.js

--- server/Index.js
+++ server/Index.js
@@ -13,7 +13,7 @@ const app = express();
 
 // IMPORTANT: CORS middleware must be one of the first middleware
 app.use(cors({
-    origin: "*",
+    origin: "https://crowd-block-d-app-frontend.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
