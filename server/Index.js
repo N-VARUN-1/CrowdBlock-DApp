bf5cd4a6d9dcd50306e140099ced426d8f26cf7f
NANDURI VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-03-12 01:21:03 +0530

    Update Index.js

--- server/Index.js
+++ server/Index.js
@@ -20,25 +20,12 @@ app.use(cors({
 }));
 
 // // OPTIONS preflight response for all routes
-// app.options('*', (req, res) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//     res.setHeader("Access-Control-Allow-Credentials", "true");
-//     res.sendStatus(200);
-// });
-
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://crowd-block-d-app-frontend.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(201);  // ✅ Respond with HTTP 200 OK for preflight
-    }
-    next();
+app.options('*', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.sendStatus(200);
 });
 
 
